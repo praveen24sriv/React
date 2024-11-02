a2c31602d37c64471997f188d6f14502be1427b5
praveen24sriv <praveen24.srivastav@gmail.com>
2024-11-02 18:44:17 +0530

    nesting elements in react

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const heading =React.createElement(
-    "h1",
-    {id:"heading"},
-    "hello react"
-);
+// const heading =React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "hello react"
+// );
 
-
+const parent= React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",
+        {id:"child"},
+        React.createElement("h1",
+            {id:"head"},
+            "nesting hori h bhyi tagdi"
+        )))
+            
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-root.render(heading);
+root.render(parent);
