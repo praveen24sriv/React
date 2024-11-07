41f1bc44a0fa421f7c51bdf42f86de14f204fe01
praveen24sriv <praveen24.srivastav@gmail.com>
2024-11-07 17:34:57 +0530

    fixing code

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent= React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div",
-        {id:"child"},
-        [React.createElement("h1",
-            {id:"head"},
-            "nesting hori h bhyi tagdi"
-        ),React.createElement("h2",
-            {id:"head"},
-            "nesting hori h bhyi tagdi hmmm"
-        )]))
-            
+
+const heading = React.createElement("h1",{},"praveen didnt slay");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//to make sibling create an array in third argument of createElement
-
-
-root.render(parent);
+root.render(heading);
