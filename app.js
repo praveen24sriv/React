78baacbf79b48de5dad1f95531f6eb70a839e2e8
praveen24sriv <praveen24.srivastav@gmail.com>
2024-11-07 18:42:29 +0530

    two ways of writing func comp

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const heAD = (<h1 id="heading">
     yo yo yo slayed thru jsx
     </h1>);
 
+//React functional component
+const HeadingComponent = ()=>{
+    return <h1 className="heading">hey func comp</h1>;
+};
+// same as upar wala func .
+const HeadingComponent2 = ()=> (<h1 className="heading">
+    hey func comp</h1>);
 
 
 
