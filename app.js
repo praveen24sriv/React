853ea8e6f4a9e245720e9372a0306843ee808acb
praveen24sriv <praveen24.srivastav@gmail.com>
2024-11-07 19:27:22 +0530

    react element in react comp

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const HEA =()=>{
-    return <h2>yo yo yo slayed thru jsx</h2>
-};
+const HEA =(
+     <h2>yo yo yo slayed thru jsx</h2>
+);
 const no = 1000;
 //Component Composition
 const HeadingComponent = ()=>(
     <div id="container">
-         <HEA/>
-         {no}
-         <h1 className="heading">hey func comp</h1>
+         {HEA}
+         <h1 className="heading">{no}hey func comp</h1>
     </div>
 );
 
