9f80cccde910fca939dfe1cc972467095fed4b51
praveen24sriv <praveen24.srivastav@gmail.com>
2024-11-08 15:02:45 +0530

    introducing props

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ const Header = ()=>{
     </div>)
 };
 
-const RestaurantCard=()=>{
+const RestaurantCard=(props)=>{
     return(<div className="card">
         <img className="res-logo" alt="res-food" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/l5sjr78yn3vhopcvqgfb"></img>
-        <h3>Meghana Foods </h3>
-        <h4>Biryani , Asian , Non-veg</h4>
+        <h3>{props.resName}</h3>
+        <h4>{props.cuisine}</h4>
         <h4>4.4 stars</h4>
         <h4>38 mins </h4>
     </div>)
@@ -35,8 +35,8 @@ const Body=()=>{
         <div className="search">Search</div>
 
         <div className="Cards-cont">
-            <RestaurantCard/>
-            <RestaurantCard/>
+            <RestaurantCard resName="Meghana foods" cuisine="indian, biryani , non-veg"/>
+            <RestaurantCard resName="KFC" cuisine="burger, Fries , non-veg"/>
         </div>
 
     </div>)
