aa77a3d7a74e9bd18a8952047de85c03228b8399
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-19 23:00:14 +0530

    on click button

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,11 @@ import resList from "../utils/mockData";
 const Body=()=>{
     return(<div className="body">
          
-        <div className="search">Search</div>
+        <div className="Filter">
+            <button className="filter-btn" onClick={()=>{console.log("button clicked")}}>Top Rated Restaurants
+
+            </button>
+        </div>
 
         <div className="Cards-cont">
             {resList.map(resto => <RestaurantCard key={resto.info.id} resData={resto}/>)}
