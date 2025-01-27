e69470f2a43b4ae3cee9abedc47f222bcfa47bba
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-27 11:15:18 +0530

    adding about comp and about route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
+import {createBrowserRouter} from "react-router-dom";
+import About from "./components/About";
+
 
 
 const AppLayout = ()=>{
@@ -13,6 +16,16 @@ const AppLayout = ()=>{
 
      </div>)
  };
+ const appRouter = createBrowserRouter([
+   {
+      path: "/",
+      element: <AppLayout/>
+   },
+   {
+     path: "/about",
+     element: <About/>
+   }
+ ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout/>);
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = ()=>{
+
+    return (
+        <div className="about">
+            <h1>About</h1>
+            <p> This is a simple React app that uses React Router to navigate between pages. </p>
+        </div>
+    );
+
+};
+export default About;
