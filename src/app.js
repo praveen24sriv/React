ec7a1a41b7d86a001f5500b8d0dee15529350e40
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-27 17:21:45 +0530

    yes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
-import {createBrowserRouter , RouterProvider} from "react-router-dom";
+
 import About from "./components/About";
+import { createBrowserRouter ,RouterProvider } from "react-router-dom";
+
 
 
 
@@ -29,4 +31,5 @@ const AppLayout = ()=>{
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter}/>);
+
 
