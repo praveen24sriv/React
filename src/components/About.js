dfaedb1fab06f3014b41d75977251db21f4babee
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-28 15:56:07 +0530

    updating the state varibale in class components

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = ()=>{
         <div className="about">
             <h1>About</h1>
             <p> This is a simple React app that uses React Router to navigate between pages. </p>
-            <User name={"Praveen Raj"}/>
+          
             <UserClass name={"Praveen Raj SRIV"}/>
         </div>
     );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,16 +8,22 @@ constructor(props) {
 
     this.state={
         count:0,
-        count2:2,
     };
 
     }
 
 render() {
-    const {count,count2} = this.state;
+    const {count} = this.state;
     return <div className="user-card">
-    <h1>Count2 ={count2}</h1>
     <h1>Count ={count}</h1>
+
+     <button onClick={
+        ()=>{this.setState({
+            count:count+1
+        })}
+        
+        }>Increment</button>
+
     <h2>Name: {this.props.name}</h2>
     <h3> Location : Bangalore</h3>
     <h4> Contact : praveen24sriv</h4>
