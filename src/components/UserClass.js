ba557670e0fa43ef55521f4e994483ae7b798577
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-28 15:51:20 +0530

    multiple state variables

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,14 +7,16 @@ constructor(props) {
     super(props);
 
     this.state={
-        count:0
+        count:0,
+        count2:2,
     };
 
     }
 
 render() {
-    const {count} = this.state;
+    const {count,count2} = this.state;
     return <div className="user-card">
+    <h1>Count2 ={count2}</h1>
     <h1>Count ={count}</h1>
     <h2>Name: {this.props.name}</h2>
     <h3> Location : Bangalore</h3>
