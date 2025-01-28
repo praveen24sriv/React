57c2dacd01027d8adfa3fa198f9b24f5b129f8a7
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-28 15:47:28 +0530

    making a state variable in class components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,9 +5,16 @@ class UserClass extends React.Component {
 
 constructor(props) {
     super(props);
+
+    this.state={
+        count:0
+    };
+
     }
+
 render() {
     return <div className="user-card">
+    <h1>Count ={this.state.count}</h1>
     <h2>Name: {this.props.name}</h2>
     <h3> Location : Bangalore</h3>
     <h4> Contact : praveen24sriv</h4>
