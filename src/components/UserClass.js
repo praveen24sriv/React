3fbe12acc1b38fa86ad54c5663b9f0a5d5cc4039
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-28 15:49:16 +0530

    another way (destructured it)

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -13,8 +13,9 @@ constructor(props) {
     }
 
 render() {
+    const {count} = this.state;
     return <div className="user-card">
-    <h1>Count ={this.state.count}</h1>
+    <h1>Count ={count}</h1>
     <h2>Name: {this.props.name}</h2>
     <h3> Location : Bangalore</h3>
     <h4> Contact : praveen24sriv</h4>
