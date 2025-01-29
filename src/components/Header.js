e9c6ae48aca769c304ba4255ab2136fc206ae778
praveen24sriv <praveen24.srivastav@gmail.com>
2025-01-29 23:07:52 +0530

    online status in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus  from "../utils/useOnlineStatus";
 
 const Header = ()=>{
     
     const [btnName, setBtn] = useState("Login"); 
+    const onlineStatus = useOnlineStatus();
 
     return(<div className="header">
         <div className="logo-container">
@@ -12,9 +14,10 @@ const Header = ()=>{
         </div>
         <div className="nav-items">
              <ul>
+                <li> Online Status :{onlineStatus?"🟢":"🔴"}</li>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About Us</Link></li>
-                <li><Link to="/contact">Contact Us</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li> 
                 <li>Cart</li>
                 <button className="login-btn" onClick={()=>{
                     btnName=="Login"?setBtn("Logout"):setBtn("Login");
