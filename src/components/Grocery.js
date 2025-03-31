3b7eb6cfe6ef11a758ca967363647d950aae46d0
praveen24sriv <praveen24.srivastav@gmail.com>
2025-03-31 12:41:18 +0530

    collapsible accordian fixed and optimised

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -9,9 +9,13 @@ const Section =({title,description,isVisible ,setIsVisible})=>{
         </h3>
         {
             isVisible? 
-            <button className="cursor-pointer underline" onClick={()=>{setIsVisible(false)}}>Hide </button>
+            <button className="cursor-pointer underline" onClick={()=>{setIsVisible(false)}}>
+                Hide 
+                </button>
             :
-            <button className="cursor-pointer underline" onClick={()=>{setIsVisible(true)}}>Show </button>
+            <button className="cursor-pointer underline" onClick={()=>{setIsVisible(true)}}>
+                Show 
+                </button>
         }
         
        
@@ -25,11 +29,7 @@ const Section =({title,description,isVisible ,setIsVisible})=>{
 
 
 const Grocery=()=>{
-    const [sectionConfig ,setSectionConfig] = useState({
-        showAbout:false,
-        showCareers:false,
-        showPress:false
-    });
+    const [VisibleConfig ,setVisibleConfig] = useState("press");
     return(
         <div>
             <h1 className="text-3xl font-bold p-2 m-2 ">INSTAMART</h1>
@@ -38,13 +38,8 @@ const Grocery=()=>{
             title ={"About Instamart"}
 
              description={"here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
-             isVisible={sectionConfig.showAbout}
-             setIsVisible={()=>setSectionConfig({
-                showAbout:true,
-                showCareers:false,
-                showPress:false
-
-             })
+             isVisible={VisibleConfig==="about"}
+             setIsVisible={()=>{setVisibleConfig(VisibleConfig === "about" ? null : "about")}
 
              }/>
 
@@ -53,30 +48,20 @@ const Grocery=()=>{
             title ={"Careers Instamart"} 
 
             description={"here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
-            isVisible={sectionConfig.showCareers}
-            setIsVisible={()=>setSectionConfig({
-                showAbout:false,
-                showCareers:true,
-                showPress:false
+            isVisible={VisibleConfig==="careers"}
+            setIsVisible={()=>setVisibleConfig(VisibleConfig==="careers"?null:"careers")
 
-             })
-
-             }/>
+            }/>
 
             <Section 
 
             title ={"Press Instamart"} 
 
             description={"here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
-            isVisible={sectionConfig.showPress}
-            setIsVisible={()=>setSectionConfig({
-                showAbout:false,
-                showCareers:false,
-                showPress:true
+            isVisible={VisibleConfig==="press"}
+            setIsVisible={()=>setVisibleConfig(VisibleConfig==="press"?null:"press")
 
-             })
-
-             }/>
+            }/>
         </div>
     )
 }
