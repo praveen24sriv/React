9c324257fcbf9d1d37421c881f996045cd5bebba
praveen24sriv <praveen24.srivastav@gmail.com>
2025-05-14 01:43:08 +0530

    unit testing 2

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,9 +1,17 @@
 import Header from "../Header";
 import { render, screen } from "@testing-library/react";
+import {Provider } from "react-redux";
+import store from "../../store";
+import { StaticRouter } from "react-router-dom/server";
 
 test ("Logo should load on rendering header", () => {
 
-    const header = render(<Header/>);
+    const header = render(<StaticRouter>
+    < Provider store={store}>
+        <Header/>
+    </Provider>
+    </StaticRouter>
+);
 
-    const logo = screen.getByAltText(/logo/i);
+    
 })
